76f1149a727911096b1e6d90c41b60554b09bba5
Abulajiang Yibulayin <abulajia@utscad.utsc.utoronto.ca>
2024-02-13 21:02:28 -0500

    minor changes

--- NerveOfSteel.py
+++ NerveOfSteel.py
@@ -8,16 +8,14 @@ import random # random libary to generate random time between the required time 
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
+im = Image.open("times-up.jpeg")
+randomtime = random.randint(5, 25)
+
 print('Welcome to Nerve of Steel\nGame Rule: Last to sit down wins')
 
 print('Players Stand')
 
-
 print(input('Press Enter to Start'))
-im = Image.open("times-up.jpeg")
-
-
-randomtime = random.randint(5, 25)
 
 time.sleep(randomtime)
 
