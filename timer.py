5063143b359409c32fb000c191063f8345f6dd18
Abulajiang Yibulayin <abulajia@utscad.utsc.utoronto.ca>
2024-02-13 19:58:35 -0500

    Ibrahim's edited version for in_class_exercise4

--- timer.py
+++ timer.py
@@ -1,23 +1,26 @@
 """
-This program is used as an example for MGTC28.
-timer.py is a simple Python script that will allow user to set timer duration.
-Upon timer expiry, user will see the time up meme and sound notification.
-timer.py uses the time library to help keep track of time
+This program is for in-class exercise 4 for MGTC28 by Ibrahim
+
 """
 
 
-# This program is timer that counts down
-
-
+import random # random libary to generate random time between the required time frame
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
+print('Welcome to Nerve of Steel\nGame Rule: Last to sit down wins')
+
+print('Players Stand')
+
+
+print(input('Press Enter to Start'))
 im = Image.open("times-up.jpeg")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
 
-time.sleep(set_time)
+randomtime = random.randint(5, 25)
+
+time.sleep(randomtime)
 
 im.show()
+
 
