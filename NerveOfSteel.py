5ca366551c03274e1a758d61f374a3272bc24ddd
Omar Khalid <omar.khalid@mail.utoronto.ca>
2024-03-09 15:35:48 -0500

    practice

--- NerveOfSteel.py
+++ NerveOfSteel.py
@@ -5,7 +5,7 @@ This program is for in-class exercise 4 for MGTC28 by Ibrahim
 
 
 import random # random libary to generate random time between the required time frame
-import time # The time library has a sleep function that will pause the script for a specifized amount of time
+import time # The time library has a sleep function that will pause the script for a specifized amount of time, practice
 from PIL import Image # the pillow library makes it easy to display images 
 
 im = Image.open("times-up.jpeg")
